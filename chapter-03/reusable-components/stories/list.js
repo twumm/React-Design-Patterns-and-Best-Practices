c9c862fbf81c29625eab0ed33c493eaaee7dd28a
Martin Twum Mensah <martint.mensah@gmail.com>
2018-12-22 12:55:11 +0000

    chore: Update story with post variables.

--- chapter-03/reusable-components/stories/list.js
+++ chapter-03/reusable-components/stories/list.js
@@ -3,6 +3,18 @@ import { storiesOf } from '@kadira/storybook'
 import List from '../src/components/list'
 
 const users = [{ id: 1, username: 'username', bio: 'bio' }]
+const posts = [
+  {
+    id: 1,
+    title: 'Create Apps with no Configuration',
+    excerpt: 'Create React App is a new officially supported...',
+  },
+  {
+    id: 2,
+    title: 'Mixins Considered Harmful',
+    excerpt: 'How do I share the code between several...',
+  },
+]
 
 storiesOf('List', module)
   .add('with bio', () => (
@@ -10,4 +22,12 @@ storiesOf('List', module)
   ))
   .add('without bio', () => (
     <List collection={users} titleKey="username" />
+  ))
+
+storiesOf('List', module)
+  .add('without text field', () => (
+    <List collection={posts} titleKey="title" />
+  ))
+  .add('with text field', () => (
+    <List collection={posts} titleKey="title" textKey="excerpt" />
   ))
